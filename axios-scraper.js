76aa9725bb5061e5322471d120576a976b0cb972
broop <soundwanders@gmail.com>
2021-03-11 21:53:41 -0500

    find a way to create unique keys for each team name. Scores have same generic tag and no class names so hard to not grab both at once

--- axios-scraper.js
+++ axios-scraper.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const url = 'https://www.sportsbettingdime.com/nhl/results';
 
+
 axios(url)
   .then(response => {
     const html = response.data;
@@ -20,17 +21,17 @@ axios(url)
     // Loop through table and extract team name and number of goals scored
     scoreboard.each(function () {
       const homeTeam = $(this).find('div > .odds-content__team-vertical-home').text();
-      const homeScore = $(this).find('td > b').text();
       const awayTeam = $(this).find('div > .odds-content__team-vertical-away').text();
-      const awayScore = $(this).find('td > b').text();
+      const score = new Set($(this).find('td > b').text());
 
-      gameData.push({
-        home: homeTeam,
-        away: awayTeam,
-        homeTotal: homeScore,
-        awayTotal: awayScore
-      });
+      gameData.push ({
+        homeTeam,
+        awayTeam,
+        goals: score,
+        awaygoals: score,
+      })
     });
+    console.log("Data below scraped from sportsbettingdime.com")
     console.log(gameData);
   })
   .catch(console.error);
